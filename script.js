4bafe8eac1abf31ce495b487f5a1a98efb8e60a2
Igor-Lima07 <igoraraujo276a@gmail.com>
2026-01-11 22:51:38 -0300

    Adiciona um js bem simples para as funçoes basicas

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-// Variaveis dos numeros
-
-const num1Btn = document.getElementById("num1Btn");
-const num2Btn = document.getElementById("num2Btn");
-const num3Btn = document.getElementById("num3Btn");
-const num4Btn = document.getElementById("num4Btn");
-const num5Btn = document.getElementById("num5Btn");
-const num6Btn = document.getElementById("num6Btn");
-const num7Btn = document.getElementById("num7Btn");
-const num8Btn = document.getElementById("num8Btn");
-const num9Btn = document.getElementById("num9Btn");
-const num0Btn = document.getElementById("num0Btn");
-
-// Variaveis dos operadores
-const plusBtn = document.getElementById("plusBtn");
-const minusBtn = document.getElementById("minusBtn");
-const divideBtn = document.getElementById("divideBtn");
-const multiplyBtn = document.getElementById("multiplyBtn");
-const equalBtn = document.getElementById("equalBtn");
-const cBtn = document.getElementById("cBtn");
-const ceBtn = document.getElementById("ceBtn");
-
 // Variavel do display
 const displayText = document.getElementById("displayText")
 
 // Logica da calculadora
 
-function main () {
+function appendToDisplay (input) {
+    displayText.value += input;
+    
+}
+
+function clearDisplay() {
+    displayText.value = '';
+}
+
+function calculate() {
+    displayText.value = eval(displayText.value)
 }
